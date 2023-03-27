88d01647cb0ee3f6b59427b698545c49b74212f7
@猪锈氢 <1667834841@qq.com>
2023-03-27 10:39:06 +0800

    添加prompt链接 加粗

--- src/system.ts
+++ src/system.ts
@@ -12,7 +12,7 @@ export const message = `
 ## 功能
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。
 - [文字转音频](https://voice.toolkit.show/)：对话右上角有播放按钮即可播放
-- **[prompt市场](https://gpt.toolkit.show/)：提供热门，有效的prompt，帮你在各个方面提高效率**
+- **[prompt市场](https://ai.toolkit.show/)：提供热门，有效的prompt，帮你在各个方面提高效率**
 
 **非常感谢所有已提供apikey的用户**
 - 至理名言
